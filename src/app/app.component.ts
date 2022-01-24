f7a2e0dddfeb3cc464434b5ad4e1738c1467dc94
kodm1812 <konarev.dmitry01@mail.ru>
2022-01-24 23:34:39 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component, VERSION } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, VERSION, OnInit } from '@angular/core';
+
+export interface GameGenre {
+  id: number;
+  name: string;
+}
+
+export interface Game {
+  id: number;
+  name: string;
+  genre: GameGenre;
+  personalRating: number;
+}
+
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit  {
+  public games: Game[] = [];
+
+  constructor(private client: HttpClient){}
+  ngOnInit(){
+    this.client.get<Game[]>('https://localhost:5001/api/games')
+      .subscribe(result => this.games = result);
+  }
 }
